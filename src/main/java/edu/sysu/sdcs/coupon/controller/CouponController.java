f2d14bc1923781e9d8e99a21ec3bd584f3850cd5
yogi <yogiwuproxy@gmail.com>
2019-12-02 11:39:35 +0800

    add swagger support

--- src/main/java/edu/sysu/sdcs/coupon/controller/CouponController.java
+++ src/main/java/edu/sysu/sdcs/coupon/controller/CouponController.java
@@ -2,6 +2,9 @@ package edu.sysu.sdcs.coupon.controller;
 
 import edu.sysu.sdcs.coupon.entity.Coupon;
 import edu.sysu.sdcs.coupon.service.CouponService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -11,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
 
+@Api(value = "优惠券Controller")
 @RestController
 @RequestMapping("/coupons")
 @Slf4j
@@ -18,6 +22,8 @@ public class CouponController {
     @Autowired
     CouponService couponService;
 
+    @ApiOperation("新增优惠券")
+    @ApiImplicitParam(name = "coupon", value = "优惠券实体", required = true, dataType = "Coupon")
     @PostMapping()
     public boolean addCoupon(@Valid @RequestBody Coupon coupon) {
         var res = couponService.addCoupon(coupon);
